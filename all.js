58d5dd67dbf2bb244901c1c5de6daa34ddc8f3f0
yingchangchen <s99214033@gmail.com>
2024-04-10 15:39:59 +0800

    新增

--- all.js
+++ all.js
@@ -3,4 +3,39 @@ $(document).ready(function () {
     e.preventDefault();
     $(".dropdown-open").slideToggle();
   });
+
+  //swiper
+  var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 3500,
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+  //lightbox
+  lightbox.option({
+    resizeDuration: 200,
+    wrapAround: true,
+  });
+
+  //top效果
+  $(".top a").click(function (e) {
+    e.preventDefault();
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
 });
